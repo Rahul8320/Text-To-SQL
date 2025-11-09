3718dec8ee2a42c6f6aa25bcebd731210f281cb5
Rahul Dey <rahuldey832000@gmail.com>
2025-11-09 16:11:29 +0530

    Init project using UV

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Text To SQL
+
+Interact with your database by naturel english text or query.
+
---
+++ main.py
@@ -0,0 +1,9 @@
+def main():
+    print("Hello from text to SQL!")
+    print(
+        "Your preferred way to interact with your database by simple natural language."
+    )
+
+
+if __name__ == "__main__":
+    main()
